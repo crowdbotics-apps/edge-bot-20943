b860d64f51ceb44924bc329e14c9f2d4eb7c5347
Crowdbotics <team@crowdbotics.com>
2020-10-01 17:08:46 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/edge_bot_20943/MainActivity.java
@@ -0,0 +1,15 @@
+package com.edge_bot_20943;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "edge_bot_20943";
+    }
+}
