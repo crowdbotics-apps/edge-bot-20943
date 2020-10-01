e727d3560ef6d6c6d3ad4b36c4554ed96245be30
Crowdbotics <team@crowdbotics.com>
2020-10-01 17:12:35 +0000

    Update models

--- backend/edge_bot_20943/wsgi.py
+++ backend/edge_bot_20943/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edge_bot_20943.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edge_bot_20943.settings")
 
 application = get_wsgi_application()
